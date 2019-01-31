e8d4530f97ace168c8dbe99149b715d5e2ba7910
Steve Lipton <steve@makeapppie.com>
2019-01-31 05:15:57 -0600

    Initial Commit

---
+++ ClosureExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClosureExercise
+//
+//  Created by Steven Lipton on 1/31/19.
+//  Copyright © 2019 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
